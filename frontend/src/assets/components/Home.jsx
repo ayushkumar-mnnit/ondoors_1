cbefe1fe4d9606b1c950a6053f9acf2b9a9efede
ayushkumar-mnnit <ayushk.2125@gmail.com>
2024-10-29 13:30:21 +0530

    updates

--- frontend/src/assets/components/Home.jsx
+++ frontend/src/assets/components/Home.jsx
@@ -13,8 +13,16 @@ const Home = () => {
     const {loading} = useAuth();
 
     if (loading) {
-        return <div className="lds-ripple"><div></div><div></div></div>
+        return (
+            <>
+                <div className="lds-ripple"><div></div><div></div></div>
+                <p style={{ color: 'red', textAlign: 'center' }}>
+                    The site is currently hosted on a free server, so it may take some time to load
+                </p>
+            </>
+        );
     }
+    
 
     return (
    <>
