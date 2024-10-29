98316159dec1521b5ef32c77a11a0cf3298dc454
ayushkumar-mnnit <ayushk.2125@gmail.com>
2024-10-29 14:14:31 +0530

    update

--- frontend/src/assets/components/Home.jsx
+++ frontend/src/assets/components/Home.jsx
@@ -13,7 +13,10 @@ const Home = () => {
     const {loading} = useAuth();
 
     if (loading) {
-        return <div className="lds-ripple"><div></div><div></div></div>
+        return ( <>
+            <p style={{textAlign:"center",color:"red"}}>The server is currently on a <span color="purple"> free plan</span> that may sleep during inactivity, so it might take at most <span color="purple" > 90 seconds </span> to wake up.</p>
+            <div className="lds-ripple"><div></div><div></div></div>
+        </>)
     }
 
     return (
